9025fbd3ec4b890a877c0121a01f4f0eb5b0c0c4
Kevin Zhang <kevinzhang19870314@gmail.com>
2023-08-24 10:50:16 +0800

    Update my_plugin.py

--- my_plugin.py
+++ my_plugin.py
@@ -1,34 +1,40 @@
-from mad_hatter.decorators import tool, hook
-from pydantic import BaseModel
-from datetime import datetime, date
-
-
-class MySettings(BaseModel):
-    required_int: int
-    optional_int: int = 69
-    required_str: str
-    optional_str: str = "meow"
-    required_date: date
-    optional_date: date = 1679616000
+import langchain
+from langchain.docstore.document import Document
+from mad_hatter.decorators import hook
+from log import log
 
 
 @hook
-def plugin_settings_schema():
-    return MySettings.schema()
+def before_rabbithole_stores_documents(docs, bot):
+    summarization_prompt = """Write a concise summary of the following:
+    {text}
+    """  # TODO make this a setting
+    summarization_chain = langchain.chains.LLMChain(
+        llm=bot._llm,
+        verbose=False,
+        prompt=langchain.PromptTemplate(template=summarization_prompt,
+                                        input_variables=["text"]),
+    )
 
+    log(f"Starting to summarize {len(docs)}", "WARNING")
+    # we will store iterative summaries all together in a list
+    all_summaries = []
 
-@tool
-def get_the_day(tool_input, bot):
-    """Get the day of the week. Input is always None."""
+    summarization_level = 0  # 0 means hierarchical?  # TODO make this a setting
+    group_size = 5  # TODO make this a setting
+    separator = "\n --> "
 
-    dt = datetime.now()
+    # make summaries of groups of docs
+    for i in range(0, len(docs), group_size):
+        group = docs[i: i + group_size]
+        group = list(map(lambda d: d.page_content, group))
 
-    return dt.strftime('%A')
+        text_to_summarize = separator + separator.join(group)
+        summary = summarization_chain.run(text_to_summarize)
+        summary = Document(page_content=summary)
+        summary.metadata["is_summary"] = True
 
+        # add summary to list of all summaries
+        all_summaries.append(summary)
 
-@hook
-def before_bot_sends_message(message, bot):
-    prompt = f'Rephrase the following sentence in a grumpy way: {message["content"]}'
-    message["content"] = bot.llm(prompt)
-
-    return message
+    return all_summaries
